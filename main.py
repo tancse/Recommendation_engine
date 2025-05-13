80f8d205414ccf85ecd1b28087fd620ff8c916a5
tancse <thananyamurugavel@gmail.com>
2025-05-13 16:35:36 +0530

    changes to model

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ class QueryInput(BaseModel):
     keyword: str | None = None
 
 @app.get("/recommend/{query}")
-def read_item(query: str, offset : int = Query(0, alias="page", ge=0), top_k : int = Query(10 , le = 50)):
+def read_item(query: str, offset : int = Query(0, alias="page", ge=0), top_k : int = Query(10 , le = 50), q: Union[str, None] = None):
     results = rank_products(query, offset, top_k)
     return {"results" : results, "offset" : offset, "to_k" : top_k}
